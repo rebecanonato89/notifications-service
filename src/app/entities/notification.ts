d2d5c58e28992a1b1668b801978c14a2b5088a74
Rebeca Nonato Domingos <rebecanonato89@gmail.com>
2022-12-15 13:14:46 -0300

    post

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -1,3 +1,5 @@
+import { Replace } from '@helpers/Replace';
+import { randomUUID } from 'node:crypto';
 import { Content } from './content';
 
 export interface NotificationProps {
@@ -9,10 +11,19 @@ export interface NotificationProps {
 }
 
 export class Notification {
+  private _id: string;
   private props: NotificationProps;
 
-  constructor(props: NotificationProps) {
-    this.props = props;
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this._id = randomUUID();
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  public get id() {
+    return this._id;
   }
 
   public get recipientId(): string {
---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,11 @@
+import { SendNotification } from '@app/use-cases/send-notification';
+import { DatabaseModule } from '@infra/database/database.module';
+import { Module } from '@nestjs/common';
+import { NotificationsController } from './controllers/notifications.controller';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [NotificationsController],
+  providers: [SendNotification],
+})
+export class HttpModule {}
